2816217e44a1dd7d9a7d40741191436748318938
Blair Learn <blairlearn@users.noreply.github.com>
2017-12-07 15:37:22 -0500

    Incorporate actual types video and detail

--- src/app/dashboard/video-thumbnail/video-thumbnail.component.ts
+++ src/app/dashboard/video-thumbnail/video-thumbnail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import {Video} from './../../api-types'
+
 @Component({
   selector: 'video-thumbnail',
   templateUrl: './video-thumbnail.component.html',
@@ -7,7 +9,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class VideoThumbnailComponent implements OnInit {
 
-  @Input("SelectedVideo") video : any[];
+  @Input("SelectedVideo") video : Video[];
 
   constructor() {
    }
